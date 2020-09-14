0f52403a25453e6dd873309782ed8e8111e9d763
babausse <courtois.vincent@outlook.com>
2020-09-14 14:02:32 +0200

    Fix rubocop issues

--- controllers/conversations.rb
+++ controllers/conversations.rb
@@ -1,6 +1,12 @@
+# frozen_string_literal: true
+
+# Main module for all the controllers of the web application
+# @author Vincent Courtois <courtois.vincent@outlook.com>
 module Controllers
+  # Insert more informations about your controller here.
+  # @author Vincent Courtois <courtois.vincent@outlook.com>
   class Conversations < Virtuatable::Controllers::Base
-    api_route 'get', '/',{authenticated: false} do
+    api_route 'get', '/', { authenticated: false } do
       api_ok 'Hello world !'
     end
   end
